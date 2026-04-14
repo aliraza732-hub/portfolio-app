510fbd98b796529086d09cca82cbe965abddd835
aliraza732-hub <alirazamehar732@gmail.com>
2026-04-14 10:07:25 +0500

    fix the typescript error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 // import { DM_Sans, DM_Mono, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import SkipLink from "./components/SkipLink";
